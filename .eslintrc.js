66146d7fdac5969c2e49bced62a1bd33f10d0ee5
Nick Taylor <nick@iamdeveloper.com>
2021-07-30 23:28:33 -0400

    Added eslint and eslint configuration.

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+};
