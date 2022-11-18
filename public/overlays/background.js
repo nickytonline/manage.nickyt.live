0992d1c3d7f570e3ccb2e250746487248ae87bf7
Nick Taylor <nick@iamdeveloper.com>
2022-11-17 20:06:16 -0500

    chore: refactored edge function for guest info

--- public/overlays/background.js
+++
@@ -1,7 +0,0 @@
-if (document.location.search) {
-  document.forms['guestForm'].style.display = 'none';
-
-  const { searchParams } = new URL(document.location.href);
-  document.getElementById('title').innerText = searchParams.get('title');
-  document.getElementById('guest').innerText = searchParams.get('guest');
-}
